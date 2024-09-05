aa1fe524a8dc897948f640126dc71870bd492dc7
lucas <lucash100souza@gmail.com>
2024-09-05 01:38:02 -0300

    feat(singlethreaded): set up module for single-threaded API
    - Added new module for implementing single-threaded API.
    - Includes initial configuration and necessary dependencies for single-threaded operation.

---
+++ api-singlethreaded/src/main/java/com/benchmark/apisinglethreaded/ApiSinglethreadedApplication.java
@@ -0,0 +1,13 @@
+package com.benchmark.apisinglethreaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSinglethreadedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSinglethreadedApplication.class, args);
+    }
+
+}
