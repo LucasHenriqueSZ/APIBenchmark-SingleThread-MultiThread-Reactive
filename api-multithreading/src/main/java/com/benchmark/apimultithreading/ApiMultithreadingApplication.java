dcead0c29ad5a2a113874eda1618f20bc0636920
lucas <lucash100souza@gmail.com>
2024-09-05 01:32:43 -0300

    feat(multithreading): set up module for multithreaded API
    - Added new module for implementing multithreaded API.
    - Includes initial configuration and dependencies for concurrent processing.

---
+++ api-multithreading/src/main/java/com/benchmark/apimultithreading/ApiMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.benchmark.apimultithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMultithreadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMultithreadingApplication.class, args);
+    }
+
+}
---
+++ api-multithreading/src/test/java/com/benchmark/apimultithreading/ApiMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benchmark.apimultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMultithreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
